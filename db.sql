0a8ffd91255370f1466dd66e64ecdb6eb42d8ad8
Даниил Свириденко <sviridenko-02@mail.ru>
2022-03-19 22:19:59 +0400

    Основа бд

--- db.sql
+++ db.sql
@@ -1,8 +1,7 @@
 use master;
 GO
-
     /*** Создание базы данных ***/
-if DB_ID('Сollege') is NOT NULL
+if DB_ID('College') is NOT NULL
 	drop database College;		
 GO
 create database College;			
@@ -11,26 +10,67 @@ GO
 use College;
 
 	/*** Создание таблицы Specialties ***/
-IF EXISTS (SELECT * FROM College.Tables WHERE NAME = 'Specialties')
+IF EXISTS (SELECT * FROM sys.tables WHERE NAME = 'Specialties')
 	DROP TABLE Specialties;						
 GO
 CREATE TABLE Specialties
-(	SpecialtyID 	    VARCHAR(8)	   	NOT NULL,	       
-	SpecialtyName       NVARCHAR(150)   NOT NULL,			
-    SpecialtyReduction  NVARCHAR(3)     NOT NULL,       
+(	SpecialtyID       NVARCHAR(3)       NOT NULL,			
+    SpecialtyName     NVARCHAR(150)     NOT NULL,     
+    SpecialtyCode     NVARCHAR(8)       NOT NULL,
 	CONSTRAINT PK_Specialties
-		PRIMARY KEY (SpecialtyCode)
+		PRIMARY KEY (SpecialtyID)
 );
 GO
 
 	/*** Создание таблицы Groups ***/
-IF EXISTS (SELECT * FROM College.Tables WHERE NAME = 'Groups')
+IF EXISTS (SELECT * FROM sys.tables WHERE NAME = 'Groups')
 	DROP TABLE Groups;						
 GO
 CREATE TABLE Groups
-(	GroupSpecialtyID    VARCHAR(8)	   	NOT NULL,	     
-	GroupCode           NVARCHAR(150)   NOT NULL,	
+(   GroupID         INT             NOT NULL IDENTITY,
+	SpecialtyID     NVARCHAR(3)     NOT NULL,	     
+	GroupNumber     NVARCHAR(3)     NOT NULL,	
 	CONSTRAINT PK_Groups
-		PRIMARY KEY (SpecialtyCode)
+		PRIMARY KEY (GroupID),
+    CONSTRAINT FK_Groups_Specialties
+        FOREIGN KEY (SpecialtyID)
+            REFERENCES Specialties (SpecialtyID)
+);
+GO
+
+	/*** Создание таблицы Genders ***/
+IF EXISTS (SELECT * FROM sys.tables WHERE NAME = 'Genders')
+	DROP TABLE Genders;						
+GO
+CREATE TABLE Genders
+(   GenderID       CHAR(1)          NOT NULL,
+	GenderName     VARCHAR(8)       NOT NULL,	     
+	CONSTRAINT PK_Genders
+		PRIMARY KEY (GenderID),
+);
+GO
+
+
+	/*** Создание таблицы Students ***/
+IF EXISTS (SELECT * FROM sys.tables WHERE NAME = 'Students')
+	DROP TABLE Students;						
+GO
+CREATE TABLE Students
+(	StudentID     INT	   	NOT NULL IDENTITY,
+    Surname       NVARCHAR(50)      NOT NULL,	     
+	FirstName     NVARCHAR(50)      NOT NULL,	
+    LastName      NVARCHAR(50),
+    GroupID       INT               NOT NULL,
+    GenderID      CHAR(1)           NOT NULL,
+    GPA           FLOAT(2)  CHECK (GPA >= 1 AND GPA <=5),
+
+	CONSTRAINT PK_Students
+		PRIMARY KEY (StudentID),
+    CONSTRAINT FK_Students_Genders
+        FOREIGN KEY (GenderID)
+            REFERENCES Genders (GenderID),
+    CONSTRAINT FK_Students_Groups
+        FOREIGN KEY (GroupID)
+            REFERENCES Groups (GroupID)
 );
 GO
